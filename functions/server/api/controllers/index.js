7e4c198dc02718c3159c45bd07c7a8803b64f122
Jose Sal y Rosas <infojasyrc@gmail.com>
2019-09-15 22:34:05 -0500

    Added nodemon and improved urls

--- functions/server/api/controllers/index.js
+++ functions/server/api/controllers/index.js
@@ -6,10 +6,10 @@ const router = express.Router();
 router.use('/healthcheck', require('./healthcheck'));
 router.use('/attendees', require('./attendees'));
 router.use('/users', require('./users'));
-router.use('/authenticate/', require('./authentication'));
-router.use('/roles/', require('./roles'));
-router.use('/headquarters/', require('./headquarters'));
-router.use('/events/', require('./events'));
-router.use('/image/', require('./image'));
+router.use('/authenticate', require('./authentication'));
+router.use('/roles', require('./roles'));
+router.use('/headquarters', require('./headquarters'));
+router.use('/events', require('./events'));
+router.use('/image', require('./image'));
 
 module.exports = router;
