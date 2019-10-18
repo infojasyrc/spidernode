ab35ac10f698207a2c126dd2f8808437e4470ac9
Jose Sal y Rosas <infojasyrc@gmail.com>
2019-10-18 12:59:16 -0500

    Added token controller

--- functions/server/api/controllers/token/token.controller.js
+++ functions/server/api/controllers/token/token.controller.js
@@ -4,12 +4,20 @@ const setupBaseController = require('./../base.controller');
 const serviceContainer = require('./../../../database/service.container');
 
 let baseController = new setupBaseController();
-const dbService = serviceContainer('token');
+const dbService = serviceContainer('authCode');
 
 let responseCode;
 let responseData;
 
 const accessToken = async (request, response) => {
+  if (request.body.gran_type === 'authorization_code') {
+    return dbService.getAccessTokenByAuthCode(request.body.code);
+  } else if (request.body.gran_type === 'refresh_token') {
+    return dbService.getAccessTokenByRefreshToken(request.body.refresh_token);
+  } else {
+    return response.send(404);
+  }
+
   return response.status(responseCode).json(responseData);
 };
 
