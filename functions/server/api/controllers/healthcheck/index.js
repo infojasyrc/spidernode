3a5a4e729d22b6ee7586945070261722f4b7b268
Jose Sal y Rosas <infojasyrc@gmail.com>
2020-04-10 22:44:16 -0500

    Removed server folder for submodule replacement

--- functions/server/api/controllers/healthcheck/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const express = require('express');
-const healthcheckController = require('./healthcheck.controller');
-const router = express.Router();
-
-router.get('/', healthcheckController.check);
-
-module.exports = router;
