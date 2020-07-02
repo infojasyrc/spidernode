0fd0b6a9c9fde909097199e40170a18968b69e72
Jose Sal y Rosas <infojasyrc@gmail.com>
2020-07-02 02:07:44 -0500

    Added integration tests

---
+++ functions/test/app.events.spec.js
@@ -0,0 +1,53 @@
+'use strict'
+
+const request = require('supertest')
+const assert = require('chai').assert
+
+const test = require('firebase-functions-test')({}, '../server/services-config/app.prod.json');
+
+let allFunctions;
+
+describe('Events', () => {
+  before(() => {
+    allFunctions = require('../index')
+  });
+
+  after(() => {
+    test.cleanup()
+  })
+
+  describe('Do List events', () => {
+    it('should get all events', done => {
+      request(allFunctions.app)
+        .get('/v1/events')
+        .expect(200)
+        .end((error, res) => {
+          assert.equal(res.body.status, 'OK', 'Expected OK as status')
+          assert.equal(res.body.message, 'Getting all events successfully', 'Expected message to be the same')
+          assert.isAbove(res.body.data.length, 0, 'Expected a number of events')
+          if (error) throw error;
+          done()
+        })
+    })
+  })
+
+  describe('Get event information', () => {
+    it('get an error message for non existing event', done => {
+      const eventId = '123456'
+      request(allFunctions.app)
+        .get(`/v1/events/${eventId}`)
+        .expect(500)
+        .end((error, res) => {
+          assert.equal(res.body.status, 'OK', 'Expected OK as status')
+          assert.equal(res.body.message, `Error getting event ${eventId} information`, 'Expected message to be the same')
+          if (error) throw error
+          done()
+        })
+    })
+  })
+
+})
+
+
+
+
